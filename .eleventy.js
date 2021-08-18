8f8d297a219bed8b61b236b13d288bc84bd318d2
Peter deHaan <peter@deseloper.com>
2021-08-18 07:32:41 -0700

    Custom Nunjucks env w/ fixed includes path

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const Nunjucks = require('nunjucks');
+
 module.exports = function (eleventyConfig) {
+  const nunjucksEnvironment = new Nunjucks.Environment(
+    new Nunjucks.FileSystemLoader("src/_includes")
+  );
+
+  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+
   return {
     dir: {
       input: "src",
